6c21811bee44ac9d4ac0b1bec215f7ee22f95367
SteinArildN <112626365+SteinArildN@users.noreply.github.com>
2023-03-13 10:44:51 +0100

    Update conv.go

--- conv/conv.go
+++ conv/conv.go
@@ -1,22 +1,24 @@
 package conv
 
-// Konverterer Farhenheit til Celsius
+// Konverterer Farhenheit
 func FahrenheitToCelsius(Fahrenheit float64) float64 {
-	return (Fahrenheit - 32) * (5 / 9)
+	return (Fahrenheit - 32.0) * (5.0 / 9.0)
 }
 func FahrenheitToKelvin(Fahrenheit float64) float64 {
-	return (Fahrenheit-32)*(5/9) + 273.15
+	return (((Fahrenheit - 32.0) * (5.0 / 9.0)) + 273.15 + 273.15)
 }
 
+// Konvertere celsius
 func CelsiusToFahrenheit(Celsius float64) float64 {
-	return Celsius*(9/5) + 32
+	return Celsius*(9.0/5.0) + 32.0
 }
 func CelsiusToKelvin(Celsius float64) float64 {
 	return Celsius + 273.15
 }
 
+// Konvertere kelvin
 func KelvintoFahrenheit(Kelvin float64) float64 {
-	return (Kelvin-273.15)*(9/5) + 32
+	return (Kelvin-273.15)*(9.0/5.0) + 32.0
 }
 func KelvintoCelsius(Kelvin float64) float64 {
 	return Kelvin - 273.15
